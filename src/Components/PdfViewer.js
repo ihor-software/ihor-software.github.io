bffc834bf8cfd0f7440f13e5798cd81711ddb604
ihor <ihor@192.168.100.2>
2024-12-19 14:05:12 +0200

    cv

--- src/Components/PdfViewer.js
+++ src/Components/PdfViewer.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import resume from "../files/Ihor_Resume.pdf"
 
 const PDFViewer = () => {
     return (
         <div style={{ height: "100vh" }}>
-            <iframe src="/Ihor_Resume.pdf" width="100%" height="100%" />
+            <iframe src={resume} width="100%" height="100%" />
         </div>
     );
 };
