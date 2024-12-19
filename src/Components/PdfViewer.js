02b3ad7bcd019fe4282af6a14b5fb883e51e9ac2
ihor <ihor@192.168.100.2>
2024-12-19 13:24:43 +0200

    changed cv path

--- src/Components/PdfViewer.js
+++ src/Components/PdfViewer.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import cv from "../files/Ihor_Resume.pdf"
 
 const PDFViewer = () => {
     return (
         <div style={{ height: "100vh" }}>
-            <iframe src={cv} width="100%" height="100%" />
+            <iframe src="/Ihor_Resume.pdf" width="100%" height="100%" />
         </div>
     );
 };
